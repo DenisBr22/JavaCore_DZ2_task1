d009ee8560fd7c01dc7e1fd500ae1a3f3bbcf4b5
Бродников Денис <platz198@yandex.ru>
2022-02-23 08:40:44 +0700

    Collection, Stream

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Collections.sort(intList);
+        for (Integer value : intList) {
+            if (value > 0 && value % 2 == 0) {
+                System.out.println(value);
+            }
+        }
+    }
+}
